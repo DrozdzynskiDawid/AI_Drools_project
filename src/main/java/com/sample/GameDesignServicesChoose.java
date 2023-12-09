d08f164fb574351c05a48282e1bb34838eafc24d
Dawid Drożdżyński <55097930+DrozdzynskiDawid@users.noreply.github.com>
2023-12-09 14:45:50 +0100

    Add files via upload

---
+++ src/main/java/com/sample/GameDesignServicesChoose.java
@@ -0,0 +1,61 @@
+package com.sample;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class GameDesignServicesChoose {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+    
+    public static final String viewQuestion(JFrame frame, String question, String[] options) {
+        int n = JOptionPane.showOptionDialog(frame,
+                                            question,
+                                            "Game Design Services Choose",
+                                            JOptionPane.YES_NO_OPTION,
+                                            JOptionPane.PLAIN_MESSAGE,
+                                            null,
+                                            options,
+                                            options[0]);
+                                            
+      System.out.print( question + " - " );
+
+      if (n == 0) {
+            System.out.println( options[0] );
+            return options[0];
+      } else if (n == 1) {
+           System.out.println( options[1] );
+           return options[1];
+      } else {
+    	  System.out.println( options[2] );
+    	  return options[2];
+      }
+}
+    
+    public static final boolean showResult(JFrame frame, String result) {
+    	JOptionPane.showMessageDialog(frame,
+    		    "Your career should be: " + result,
+    		    "RESULT",
+    		    JOptionPane.PLAIN_MESSAGE);
+    	
+    	System.out.println( "--------------------------------\nRESULT: " + result );
+    	return true;
+    }
+}
